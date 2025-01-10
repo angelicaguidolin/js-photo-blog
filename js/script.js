e3bacd9454a0026878494ed8b181a542e850f57e
angelica <guidolinangelica66@gmail.com>
2025-01-10 16:27:46 +0100

    feat add js code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-console.log(axios)
+//DOM ELEMENTS//
+const photoCardElem = document.getElementById("photo-card")
+
+ 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
-  .then(function (resp) {
-    // handle success
-    console.log(resp.data.url);
+  .then((resp) => {
+    const photos = resp.data; 
+    
   })
+photos.forEach((photo)=>{
+  photoCardElem.innerHTML += `
+  <section id="photo-card" class=" container row gap-30 ">
+            <div class="col">
+                <div class="photos">
+                    <div  class="thumbtack">
+                        <img src="./assets_day1/img/pin.svg" alt="">
+                    </div>
+                    <div class="photos-img">
+                        <div class="img-place-holder">
+                            <img src="" alt="">
+                        </div>
+                    </div>
+                     <div class="description">
+                            <h3>${photos.title}</h3>
+                        </div>
+                </div>
+            </div>
+        </section>
+  
+  `
+})
